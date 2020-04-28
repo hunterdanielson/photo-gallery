905c9c7259d7f8cb70735cf9f27611e66f5140fe
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-28 16:38:49 -0700

    got 2 filters working

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import './App.css';
 
 export default class App extends Component {
   state = { selected: null, filteredImages: images };
+
+  findOptions = () => {
+    const imageOptions = images.map(object => {
+      return object.horns;
+    })
+    return imageOptions;
+  }
+
   filterOptionsWord = (e) => {
-    // apparently need to change it to a number becuase if it is a string it will break
-    this.setState({ selected: Number(e.target.value) });
     this.setState({ filteredImages: images.filter(image => {
-      // before I used e.target.value here it was delayed for some reason
-      // I would pick option 1 and it would not update then pick option 2 and it would update with option 1 results
       if (!e.target.value) return true;
-      return image.horns === Number(e.target.value);
+      return image.keyword === e.target.value;
     }) });
 
   }
@@ -33,12 +37,14 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <p>Can only use one filter at a time</p>
+        <p>{this.findOptions()}</p>
         <section className="options">
           <select onChange={this.filterOptionsWord}>
             <option value="">All options</option>
-            <option value="1">1 horn</option>
-            <option value="2">2 horn</option>
-            <option value="3">3 horn</option>
+            <option value="rhino">Rhino</option>
+            <option value="narwhal">Narwhal</option>
+            <option value="unicorn">Unicorn</option>
           </select>
           <select onChange={this.filterOptions}>
             <option value="">All options</option>
@@ -48,7 +54,7 @@ export default class App extends Component {
           </select>
         </section>
 
-        <div>My stuff here</div>
+        <div>Chosen Images:</div>
         {
             <ImageList images={this.state.filteredImages}/>
            
